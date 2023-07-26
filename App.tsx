b8f4631c12aa6942e278806474e3d8a09fdd99f0
danilbutakov <ibutakov52@gmail.com>
2023-07-26 20:56:20 +0800

    initial commit

---
+++ App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text, View} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <StatusBar />
+      <View>
+        <Text>123</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+export default App;
